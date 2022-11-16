c817bfddb1e184384f7334e1dd809579763c18d0
Miguel <miguezaga2@hotmail.com>
2022-11-16 09:40:19 -0500

    style intro top mobile

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,23 +7,28 @@ import patternCircle from "/src/assets/images/pattern-circle.svg";
 export default function Intro() {
   return (
     <section className="intro">
-      <picture>
-        <source srcset={profileDesktop} media="(min-width: 1000px)" />
-        <source srcset={profileTablet} media="(min-width: 600px)" />
-        <img className="intro__img" src={profileMobile} alt="Profile picture" />
-      </picture>
-      <img src="" alt="" />
-      <h1>
-        Nice to meet you! I'm <b>Adam Keyes</b>.
-      </h1>
-      <p>
-        Based in the UK, I'm a front-end developer passionate about building
-        accessible web apps that users love.
-      </p>
+      <div className="intro__top">
+        <picture>
+          <source srcset={profileDesktop} media="(min-width: 1000px)" />
+          <source srcset={profileTablet} media="(min-width: 600px)" />
+          <img
+            className="intro__img"
+            src={profileMobile}
+            alt="Profile picture"
+          />
+        </picture>
+        <h1 className="intro__title">
+          Nice to meet you! I'm <b className="intro__title-name">Adam Keyes</b>.
+        </h1>
+        <p className="intro__text">
+          Based in the UK, I'm a front-end developer passionate about building
+          accessible web apps that users love.
+        </p>
 
-      <button>Contact me</button>
+        <button className="intro__button button">Contact me</button>
+      </div>
 
-      <div>
+      <div className="intro__bottom">
         <ul>
           <li>HTML 4 Years Experience</li>
 
